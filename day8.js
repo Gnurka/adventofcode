c7d0494c86ffdb29e94497b55ad555e5c217bf26
gustav.darpo <gustav.darpo@PC798>
2016-12-08 12:50:38 +0100

    Added day 8.

---
+++ day8.js
@@ -0,0 +1,223 @@
+var input = `rect 1x1
+rotate row y=0 by 6
+rect 1x1
+rotate row y=0 by 3
+rect 1x1
+rotate row y=0 by 5
+rect 1x1
+rotate row y=0 by 4
+rect 2x1
+rotate row y=0 by 5
+rect 2x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 5
+rect 4x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 3
+rect 1x1
+rotate row y=0 by 3
+rect 1x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 6
+rect 4x1
+rotate row y=0 by 4
+rotate column x=0 by 1
+rect 3x1
+rotate row y=0 by 6
+rotate column x=0 by 1
+rect 4x1
+rotate column x=10 by 1
+rotate row y=2 by 16
+rotate row y=0 by 8
+rotate column x=5 by 1
+rotate column x=0 by 1
+rect 7x1
+rotate column x=37 by 1
+rotate column x=21 by 2
+rotate column x=15 by 1
+rotate column x=11 by 2
+rotate row y=2 by 39
+rotate row y=0 by 36
+rotate column x=33 by 2
+rotate column x=32 by 1
+rotate column x=28 by 2
+rotate column x=27 by 1
+rotate column x=25 by 1
+rotate column x=22 by 1
+rotate column x=21 by 2
+rotate column x=20 by 3
+rotate column x=18 by 1
+rotate column x=15 by 2
+rotate column x=12 by 1
+rotate column x=10 by 1
+rotate column x=6 by 2
+rotate column x=5 by 1
+rotate column x=2 by 1
+rotate column x=0 by 1
+rect 35x1
+rotate column x=45 by 1
+rotate row y=1 by 28
+rotate column x=38 by 2
+rotate column x=33 by 1
+rotate column x=28 by 1
+rotate column x=23 by 1
+rotate column x=18 by 1
+rotate column x=13 by 2
+rotate column x=8 by 1
+rotate column x=3 by 1
+rotate row y=3 by 2
+rotate row y=2 by 2
+rotate row y=1 by 5
+rotate row y=0 by 1
+rect 1x5
+rotate column x=43 by 1
+rotate column x=31 by 1
+rotate row y=4 by 35
+rotate row y=3 by 20
+rotate row y=1 by 27
+rotate row y=0 by 20
+rotate column x=17 by 1
+rotate column x=15 by 1
+rotate column x=12 by 1
+rotate column x=11 by 2
+rotate column x=10 by 1
+rotate column x=8 by 1
+rotate column x=7 by 1
+rotate column x=5 by 1
+rotate column x=3 by 2
+rotate column x=2 by 1
+rotate column x=0 by 1
+rect 19x1
+rotate column x=20 by 3
+rotate column x=14 by 1
+rotate column x=9 by 1
+rotate row y=4 by 15
+rotate row y=3 by 13
+rotate row y=2 by 15
+rotate row y=1 by 18
+rotate row y=0 by 15
+rotate column x=13 by 1
+rotate column x=12 by 1
+rotate column x=11 by 3
+rotate column x=10 by 1
+rotate column x=8 by 1
+rotate column x=7 by 1
+rotate column x=6 by 1
+rotate column x=5 by 1
+rotate column x=3 by 2
+rotate column x=2 by 1
+rotate column x=1 by 1
+rotate column x=0 by 1
+rect 14x1
+rotate row y=3 by 47
+rotate column x=19 by 3
+rotate column x=9 by 3
+rotate column x=4 by 3
+rotate row y=5 by 5
+rotate row y=4 by 5
+rotate row y=3 by 8
+rotate row y=1 by 5
+rotate column x=3 by 2
+rotate column x=2 by 3
+rotate column x=1 by 2
+rotate column x=0 by 2
+rect 4x2
+rotate column x=35 by 5
+rotate column x=20 by 3
+rotate column x=10 by 5
+rotate column x=3 by 2
+rotate row y=5 by 20
+rotate row y=3 by 30
+rotate row y=2 by 45
+rotate row y=1 by 30
+rotate column x=48 by 5
+rotate column x=47 by 5
+rotate column x=46 by 3
+rotate column x=45 by 4
+rotate column x=43 by 5
+rotate column x=42 by 5
+rotate column x=41 by 5
+rotate column x=38 by 1
+rotate column x=37 by 5
+rotate column x=36 by 5
+rotate column x=35 by 1
+rotate column x=33 by 1
+rotate column x=32 by 5
+rotate column x=31 by 5
+rotate column x=28 by 5
+rotate column x=27 by 5
+rotate column x=26 by 5
+rotate column x=17 by 5
+rotate column x=16 by 5
+rotate column x=15 by 4
+rotate column x=13 by 1
+rotate column x=12 by 5
+rotate column x=11 by 5
+rotate column x=10 by 1
+rotate column x=8 by 1
+rotate column x=2 by 5
+rotate column x=1 by 5`;
+
+var screen = _.times(6, function (r) {
+    return new Array(50).fill(0);
+});
+
+function rect(a, b, screen) {
+    _.times(b, function (row) {
+        _.times(a, function (col) {
+            screen[row][col] = 1;
+        });
+    });
+}
+
+function rotateRow(b, times, screen) {
+    _.times(times, function () {
+        var last = screen[b].pop();
+        screen[b].unshift(last);
+    });
+}
+
+function rotateCol(a, times, screen) {
+    var transp = _.zip.apply(_, screen);
+    rotateRow(a, times, transp);
+    screen = _.zip.apply(_, transp);
+}
+
+function step1() {
+    var RECT = "rect ";
+    var ROTROW = "rotate row y=";
+    var ROTCOL = "rotate column x=";
+
+    input.split("\n").forEach(function (line) {
+        var i = 0;
+        if ((i = line.indexOf(RECT)) >= 0) {
+            var params = line.substring(i+RECT.length).split("x");
+            rect(parseInt(params[0]), parseInt(params[1]), screen);
+        }
+        else if ((i = line.indexOf(ROTROW)) >= 0) {
+            var params = line.substring(i+ROTROW.length).split(" by ");
+            rotateRow(parseInt(params[0]), parseInt(params[1]), screen);
+        }
+        else if ((i = line.indexOf(ROTCOL)) >= 0) {
+            var params = line.substring(i+ROTCOL.length).split(" by ");
+            rotateCol(parseInt(params[0]), parseInt(params[1]), screen);
+        }
+    });
+
+    var res = _.reduce(screen, function (sum, row) {
+        var rowSum = _.reduce(row, function (sum, col) {
+            return sum + col;
+        });
+
+        return sum + rowSum;
+    }, 0);
+
+    console.log(res);
+}
+
+$(document).ready(function () {
+    step1();
+});
