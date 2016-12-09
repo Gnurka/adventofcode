01810ce974e901464d3f32246d1ac1cc9399d48c
gustav.darpo <gustav.darpo@PC798>
2016-12-09 16:27:48 +0100

    Completed day9

--- day4.py
+++ day4.py
@@ -1,19 +1,44 @@
 import re
 from operator import itemgetter
 
-input = 'aczupnetwp-dnlgpyrpc-sfye-dstaatyr-561[patyc]'
-input = input.replace('-', '')
+total = 0
 
-m = re.search('\d+', input)
-id = m.group(0)
-letters = input[:m.span()[0]]
-checksum = re.search('\[\w+\]', input).group(0)[1:-1]
+lines = open("./day4.txt").readlines()
 
-dict= {}
-for key in letters:
-    dict[key] = letters.count(key)
+test = ["aaaaa-bbb-z-y-x-123[abxyz]\n", "a-b-c-d-e-f-g-h-987[abcde]\n", "not-a-real-room-404[oarel]", "totally-real-room-200[decoy]"]
 
-items = sorted(dict.items())
-items = sorted(items, key=itemgetter(1), reverse=True)
+def decode(word, shift):
+    word = word.replace('-', ' ')
+    decode = ""
+    for w in word:
+        if (w == ' '):
+            decode += ' '
+        else:
+            decode += chr((ord(w)-97 + shift)%26 + 97)
 
-print(checksum)
+    return decode
+
+for l in lines:
+    #input = 'aczupnetwp-dnlgpyrpc-sfye-dstaatyr-561[patyc]'
+    input = l.replace('-', '')
+    m = re.search('\d+', input)
+    id = int(m.group(0))
+    letters = input[:m.span()[0]]
+    checksum = re.search('\[\w+\]', input).group(0)[1:-1]
+
+    dict= {}
+    for key in letters:
+        dict[key] = letters.count(key)
+
+    items = sorted(dict.items())
+    items = sorted(items, key=itemgetter(1), reverse=True)
+
+    sortedLetters = ''.join([x[0] for x in items])
+    if (sortedLetters.find(checksum) >= 0):
+        total += id
+        dec = decode(l, id)
+        print(dec + str(id))
+
+print(total)
+
+#print(decode("qzmt-zixmtkozy-ivhz", 343))
